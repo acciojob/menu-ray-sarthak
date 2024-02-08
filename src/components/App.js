c4ec4e63473cf6d397ddf65312704898824619bb
Roshan <lalasarthakr@gmail.com>
2024-02-08 15:28:43 +0530

    commit2

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,16 @@ import Section from "./Section";
 const App=()=>{
 
     const [currentCategory,setcurrentCategory]=useState("");
- 
+    useEffect(()=>{
+        Data.map((item,index)=>(
+                
+            <Section key={index}
+              title={item.title}
+              description={item.desc}
+            />
+         
+     ))
+    },[])
 
     
 
@@ -18,17 +27,27 @@ const App=()=>{
 
              <h1>Menu</h1>
              <div className="btn">
-                <button onClick={()=>setcurrentCategory("all")}>All</button>
-                <button onClick={()=>setcurrentCategory("breakfast")}>BreakFast</button>
-                <button onClick={()=>setcurrentCategory("lunch")}>Lunch  </button>
-                <button onClick={()=>setcurrentCategory("shakes")}>Shakes</button>
+                <button id="filter-btn-1"onClick={()=>setcurrentCategory("")}>All</button>
+                <button id="filter-btn-2" onClick={()=>setcurrentCategory("breakfast")}>BreakFast</button>
+                <button  id="filter-btn-3" onClick={()=>setcurrentCategory("lunch")}>Lunch  </button>
+                <button  id="filter-btn-1"onClick={()=>setcurrentCategory("shakes")}>Shakes</button>
                 {
+
+                    currentCategory===""?
+                    Data.map((item,index)=>(
+                
+                        <Section key={index}
+                          title={item.title}
+                          description={item.desc}
+                        />
+                     
+                 ))
+                    
+                    :
             Data.map((item,index)=>(
-                currentCategory==="all"?Data.map((item,index)=><Section key={index}
-                title={item.title}
-                description={item.desc}
-              />):
-                item.category===currentCategory?
+                
+               
+                item.category===currentCategory && currentCategory!="all"?
                    <Section key={index}
                      title={item.title}
                      description={item.desc}
